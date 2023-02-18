034d6cc3dc8da539c722d932788d8eb53e06f752
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-02-18 23:29:48 +0100

    configuration of todo grid

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ document.querySelector("#enter").addEventListener("click", () => {
 //? => si ça existe
 //: => si ça existe pas alors 
 
+function displateItems(){
+    let items = ""
+    for(let i = 0 ; i < itemsArray.length; i++){ //loop the itemArray grâce au fait que tu peux pas être inférieur à 0 donc infini = alors il incrémente
+        items += `    <div class="item">
+        <div class="input-controller">
+
+            <textarea disabled>${itemsArray[i]}</textarea>
+
+            <div class="edit-controller">
+                <i class="fa-solid fa-check deleteBtn"></i>
+                <i class="fa-solid fa-pen-to-square editBtn"></i>
+            </div>
+
+        </div>
+
+        <div class="update-controller">
+            <button class="save">Save</button>
+            <button class="CancelBtn">Cancel</button>
+        </div>
+    </div>`
+    }
+    document.querySelector(".to-do-list").innerHTML = items
+}
+
 function createItem(item) {
      itemsArray.push(item.value) //Dans itemsArray on push la valeur de l'item
      localStorage.setItem("items", JSON.stringify(itemsArray)) //stringify car itemsArray contient item.value
@@ -17,11 +41,12 @@ function createItem(item) {
 function displayDate() {
     let date = new Date()
     date = date.toString().split(" ") //Separe tout car espace
-    document.querySelector('#date').innerHTML = date[1] + " " + date[2] + " " + date[3]
+    //document.querySelector('#date').innerHTML = date[1] + " " + date[2] + " " + date[3]
 }
 
 //innerHTML = insérer un élement dans la page ou modifier le contenu d'une balise
 
 window.onload = function(){ //quand le window est chargé on execute la fonction displayDate
     displayDate()
+    displateItems()
 }
